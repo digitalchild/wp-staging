09076caac345082554f0c12551929d5bbd4a8fa9
digitalchild <digitalchild@gmail.com>
2014-10-26 19:30:53 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+--- WP-Staging Creator ---
+
+This is a simple shell script that utilises WP-CLI to take a snapshot of a live database and create a completely separate staging server. This will create a new database, new database user and random password. 
+
+This system depends on the great command line utitliy WP-CLI[http://wp-cli.org]
+
+This will install into a directory called staging. This will be the webroot for the staging server. 
+
+--- USAGE ---
+ 
+Usage: mysqluser mysqlpass livedb livedoman stagingdomain 
+
+mysqluser = admin username
+mysqlpass = admin pass
+livedb = live database name 
+gitrepo = url to your git repository 
+dbprefx = wordpres database prefix 
+livedomain = current live domain name
+stagingdomain = staging domain name 
+
+--- TESTED ON ---
+
+This has been tested with MAMP Pro and MAMP on OS X 10.9.5 following the WP-CLI install instructions 
---
+++ wp-staging.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+# Create staging server from live database 
+
+# Generate a Password 
+genpasswd() {
+	local l=$1
+       	[ "$l" == "" ] && l=16
+      	openssl rand -base64 $l
+}
+
+#check that wordpress-cli is installed 
+# asssumes you have installed wp-cli into your path and renamed it to wp
+checkwpcli() {
+    if hash wp 2>/dev/null; then
+        echo 'wp-cli installed.'; 
+    else
+        echo 'wp-cli is required. Download and install available from http://wp-cli.org/'; 
+    fi
+}
+
+# Generate a database name or user 
+gendbdetails(){ 
+local l=$1
+       	[ "$l" == "" ] && l='staging'
+	DATE=`date +"%Y-%m-%d"`
+	echo $l'-'$DATE
+}
+
+# Create the database and user, grant privileges. 
+createdb() { 
+	MYSQL=`which mysql`
+	Q1="CREATE DATABASE IF NOT EXISTS  \`$1\`;"
+	Q2="GRANT USAGE ON *.* TO \`$2\`@localhost IDENTIFIED BY '$3';"
+	Q3="GRANT ALL PRIVILEGES ON \`$1\`.* TO \`$2\`@localhost;"
+	Q4="FLUSH PRIVILEGES;"
+	SQL="${Q1}${Q2}${Q3}${Q4}"
+	$MYSQL -u$4 -p$5 -e "$SQL"
+}
+
+EXPECTED_ARGS=7
+E_BADARGS=65
+
+if [ $# -ne $EXPECTED_ARGS ]
+then
+  echo "Usage: $0 mysqluser mysqlpass livedb gitrepo dbprefix livedoman stagingdomain"
+  exit $E_BADARGS
+fi
+
+# Set the Variables 
+MYSQLUSER=$1
+MYSQLPASS=$2
+LIVEDB=$3 
+GITREPO=$4
+DBUSER=`gendbdetails user`
+DBPASS=`genpasswd 8`
+DBNAME=`gendbdetails`
+DBPREFIX=$5
+OLDDOMAIN=$6
+NEWDOMAIN=$7
+
+echo '---------------------------------------------'
+echo 'WP Staging - The simple staging server script'
+echo '---------------------------------------------'
+echo 'Checking if WP-CLI is installed....'
+checkwpcli 
+echo '---------------------------------------------'
+echo 'Creating staging server.....'
+echo '---------------------------------------------'
+echo 'Creating staging web-root'
+mkdir staging;  cd staging; 
+createdb $DBNAME $DBUSER $DBPASS $MYSQLUSER $MYSQLPASS
+echo 'Database created....'
+wp core download
+rm -rf license.txt readme.html wp-config-sample.php
+echo 'WP clean up complete...'
+wp core config --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --dbhost=localhost --dbprefix=$DBPREFIX
+# These details don't matter as they will be overridden as soon as the import is done. 
+wp core install --url='$NEWDOMAIN' --title='Test' --admin_user='admin' --admin_password='password' --admin_email='test@test.com'
+echo 'Base Wordpress configuration completed....'
+echo 'Dumping live database...'
+mysqldump -u$MYSQLUSER -p$MYSQLPASS $LIVEDB > import.sql
+echo 'Importing live database into staging database .....'
+wp db import import.sql
+echo 'Removing database dump now that it has been imported....'
+rm -f import.sql
+echo 'Updating domain name....'
+wp search-replace $OLDDOMAIN $NEWDOMAIN
+mv wp-content wp-content.orig
+git clone $GITREPO wp-content
+echo '---------------------------------------------'
+echo 'Staging server created.'
+echo '---------------------------------------------'
